946ea9c8ecea08adb984d630bd5a566670477a72
yash15072008 <75427466+yash15072008@users.noreply.github.com>
2021-03-23 19:58:12 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+function preload() {
+//preload the images here
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+
+}
+
+function draw() {
+  background(0);
+
+}
+
